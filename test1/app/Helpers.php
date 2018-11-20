893e5c350b582c8a25fb371ed6b59a1c37e19882
kader <abdul.kader.dev@gmail.com>
2018-11-20 23:54:40 +0800

    minor changes

--- test1/app/Helpers.php
+++ test1/app/Helpers.php
@@ -7,14 +7,14 @@ class Helpers
     /**
      * Find corresponding closing parenthesis index by given open parenthesis index.
      *
-     * @param  $openParenIndex
+     * @param  $openParenthesisIndex
      * @param  $string
-     * @return $closeParenIndex
+     * @return $closeParenthesisIndex
      */
     public function findCorrespondingClosingParenthesis($openParenthesisIndex, $string)
     {
-        // validate open parenthesis index
-        if (($openParenthesisIndex > strlen($string) - 1) || $string[$openParenthesisIndex] != "(") {
+        // Passed $openParenthesisIndex should be less than the string length, not the last character and an index of open parenthessis.
+        if (($openParenthesisIndex > strlen($string) - 2) || ($string[$openParenthesisIndex] != "(")) {
             throw new \Exception("Invaild open parenthesis index.");
         }
 
@@ -22,7 +22,7 @@ class Helpers
         $counter = 1;
 
         while ($counter > 0) {
-            // throw exception if reach end of string without finding the corresponding closing parenthesis.
+            // Throw exception if reach end of string without finding the corresponding closing parenthesis.
             if ($closeParenthesisIndex > strlen($string) - 2) {
                 throw new \Exception("Invaild string.");
             }
